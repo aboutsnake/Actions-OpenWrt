473c94b42f426250bdbb70818f04b2abce483a2a
aboutsnake <57434384+aboutsnake@users.noreply.github.com>
2019-12-09 09:15:17 +0800

    Delete diy.sh

--- diy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
-# Modify default IP
-sed -i 's/10.10.10.47/192.168.50.5/g' package/base-files/files/bin/config_generate
